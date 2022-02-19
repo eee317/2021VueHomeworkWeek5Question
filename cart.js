ff98023d33708fa4107160d287da248696763d50
Pei <gonsux.3686@gmail.com>
2022-02-20 00:55:09 +0800

    完成modal視窗的購物車加入功能（axios.put）

--- cart.js
+++ cart.js
@@ -53,6 +53,7 @@ const app=createApp({
             axios.post(url, {data})
             .then(res=>{
                 this.getCart();
+                this.$refs.productModal.closeModal();
                 this.isLoadingItem='';
             })
             .catch(err=>{
@@ -68,6 +69,23 @@ const app=createApp({
                 this.isLoadingItem = '';
             });
         },
+        updateCartItem(item){
+            const data={
+                product_id:item.id,
+                qty:item.qty
+            }
+            this.isLoadingItem=item.id;
+            const url=`${apiUrl}/v2/api/${api_path}/cart/${item.id}`
+            axios.put(url,{data})
+            .then(res=>{
+                console.log(res)
+                this.getCart();
+                this.isLoadingItem='';
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
 
         
     },
@@ -86,7 +104,8 @@ app.component('product-modal',{
         return{
             //讓此物件可以在元件內使用this共用
             modal:{},
-            product:{}
+            product:{},
+            qty:1
         }
     },
     watch:{
@@ -100,6 +119,9 @@ app.component('product-modal',{
         openModal(){
             this.modal.show();
         },
+        closeModal(){
+            this.modal.hide();
+        },
         getProduct(){
             axios.get(`${apiUrl}/v2/api/${api_path}/product/${this.id}`)
             .then(res=>{
@@ -111,6 +133,11 @@ app.component('product-modal',{
                 console.log(err)
             })
         },
+        addToCart(){
+            this.$emit('add-cart',this.product.id, this.qty)
+        },
+        
+        
         
     },
     //抓DOM
